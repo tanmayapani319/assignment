195b937ba3ded489c28f24ef63a71fdd3de76a19
Tanmaya Pani <tanmayapani319@gmail.com>
2021-07-12 21:49:33 +0530

    Assignment-1 draft changes
    Initial changes for assignment-1 for dice problem

---
+++ dice-assignment/src/main/java/com/avaloq/diceassignment/DiceAssignmentApplication.java
@@ -0,0 +1,13 @@
+package com.avaloq.diceassignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiceAssignmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiceAssignmentApplication.class, args);
+	}
+
+}
---
+++ dice-assignment/src/test/java/com/avaloq/diceassignment/DiceAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.avaloq.diceassignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiceAssignmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
